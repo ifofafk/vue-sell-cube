86c43056e329c3a8da8a6b1fd5c7bfc0fc03670a
ifofafk <908816095@qq.com>
2019-01-02 09:17:52 +0800

    将js风格修改为符合eslint

--- src/common/js/date.js
+++ src/common/js/date.js
@@ -1,6 +1,6 @@
 export function formatDate(date, fmt) {
   if (/(y+)/.test(fmt)) {
-    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length));
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
   }
   let o = {
     'M+': date.getMonth() + 1,
@@ -19,5 +19,5 @@ export function formatDate(date, fmt) {
 };
 
 function padLeftZero(str) {
-  return ('00' + str).substr(str.length);
+  return ('00' + str).substr(str.length)
 }
